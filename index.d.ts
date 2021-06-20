f0c626e83ece10162ec6ba665d2a0be0204f7fdd
Eyas Ranjous <eyas.ranjous@gmail.com>
2021-06-20 21:14:49 +0200

    add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+import { LinkedList } from './src/linkedList';
+import { LinkedListNode } from './src/linkedListNode';
+import { DoublyLinkedList } from './src/doublyLinkedList';
+import { DoublyLinkedListNode } from './src/doublyLinkedListNode';
+
+export { LinkedList }
+export { LinkedListNode }
+export { DoublyLinkedList }
+export { DoublyLinkedListNode }
