0828f195bb89df212ac7e1a49b54ae5ca1f93028
Eyas Ranjous <eyas.ranjous@gmail.com>
2023-03-19 18:25:27 -0700

    ts

--- src/linkedListNode.d.ts
+++ src/linkedListNode.d.ts
@@ -1,8 +1,8 @@
-export class LinkedListNode<T> {
-  constructor(value?: T, next?: LinkedListNode<T>);
-  setValue(value: T): LinkedListNode<T>;
-  getValue(): T;
-  setNext(next: LinkedListNode<T>): LinkedListNode<T>;
-  getNext(): LinkedListNode<T>;
+export class LinkedListNode {
+  constructor(value?: any, next?: LinkedListNode);
+  setValue(value: any): LinkedListNode;
+  getValue(): any;
+  setNext(next: LinkedListNode): LinkedListNode;
+  getNext(): LinkedListNode;
   hasNext(): boolean;
 }
