163c808b1ff8781dd00b35f610912337f25266be
Eyas Ranjous <eyas.ranjous@gmail.com>
2024-07-12 15:57:33 -0700

    node types

--- src/doublyLinkedList.d.ts
+++ src/doublyLinkedList.d.ts
@@ -1,27 +1,27 @@
 import { DoublyLinkedListNode } from './doublyLinkedListNode';
 
-export class DoublyLinkedList<T> {
+export class DoublyLinkedList<T extends DoublyLinkedListNode> {
   constructor();
-  insertFirst(value: T | DoublyLinkedListNode): DoublyLinkedListNode;
-  insertLast(value: T | DoublyLinkedListNode): DoublyLinkedListNode;
-  insertAt(position: number, value: T | DoublyLinkedListNode): DoublyLinkedListNode;
-  insertBefore(value: T | DoublyLinkedListNode, node?: DoublyLinkedListNode): DoublyLinkedListNode;
-  insertAfter(value: T | DoublyLinkedListNode, node?: DoublyLinkedListNode): DoublyLinkedListNode;
-  removeFirst(): DoublyLinkedListNode;
-  removeLast(): DoublyLinkedListNode;
-  removeAt(position: number): DoublyLinkedListNode;
-  remove(node: DoublyLinkedListNode): DoublyLinkedListNode;
-  removeEach(cb: (node: DoublyLinkedListNode, position: number) => boolean): number;
-  forEach(cb: (node: DoublyLinkedListNode, position: number) => void): void;
-  forEachReverse(cb: (node: DoublyLinkedListNode, position: number) => void): void;
-  find(cb: (node: DoublyLinkedListNode) => boolean, startingNode?: DoublyLinkedListNode): DoublyLinkedListNode;
-  findReverse(cb: (node: DoublyLinkedListNode) => boolean, startingNode?: DoublyLinkedListNode): DoublyLinkedListNode;
-  filter(cb: (node: DoublyLinkedListNode, position: number) => boolean): DoublyLinkedList<T>;
-  head(): DoublyLinkedListNode;
-  tail(): DoublyLinkedListNode;
+  insertFirst(value: T): T;
+  insertLast(value: T): T;
+  insertAt(position: number, value: T): T;
+  insertBefore(value: T, node?: T): T;
+  insertAfter(value: T, node?: T): T;
+  removeFirst(): T;
+  removeLast(): T;
+  removeAt(position: number): T;
+  remove(node: T): T;
+  removeEach(cb: (node: T, position: number) => boolean): number;
+  forEach(cb: (node: T, position: number) => void): void;
+  forEachReverse(cb: (node: T, position: number) => void): void;
+  find(cb: (node: T) => boolean, startingNode?: T): T;
+  findReverse(cb: (node: T) => boolean, startingNode?: T): T;
+  filter(cb: (node: T, position: number) => boolean): DoublyLinkedList<T>;
+  head(): T;
+  tail(): T;
   count(): number;
-  toArray(): DoublyLinkedListNode[];
+  toArray(): T[];
   isEmpty(): boolean;
   clear(): void;
-  static fromArray<T>(values: T[] | DoublyLinkedListNode[]): DoublyLinkedList<T>;
+  static fromArray<T extends DoublyLinkedListNode>(values: T[]): DoublyLinkedList<T>;
 }
--- src/linkedList.d.ts
+++ src/linkedList.d.ts
@@ -1,21 +1,21 @@
 import { LinkedListNode } from './linkedListNode';
 
-export class LinkedList<T> {
+export class LinkedList<T extends LinkedListNode> {
   constructor();
-  insertFirst(value: T | LinkedListNode): LinkedListNode;
-  insertLast(value: T | LinkedListNode): LinkedListNode;
-  insertAt(position: number, value: T | LinkedListNode): LinkedListNode;
-  removeFirst(): LinkedListNode;
-  removeLast(): LinkedListNode;
-  removeEach(cb: (node: LinkedListNode, position: number) => boolean): number;
-  removeAt(position: number): LinkedListNode;
-  forEach(cb: (node: LinkedListNode, position: number) => void): void;
-  find(cb: (node: LinkedListNode) => boolean, startingNode?: LinkedListNode): LinkedListNode;
-  filter(cb: (node: LinkedListNode, position: number) => boolean): LinkedList<T>;
-  head(): LinkedListNode;
+  insertFirst(value: T): T;
+  insertLast(value: T): T;
+  insertAt(position: number, value: T): T;
+  removeFirst(): T;
+  removeLast(): T;
+  removeEach(cb: (node: T, position: number) => boolean): number;
+  removeAt(position: number): T;
+  forEach(cb: (node: T, position: number) => void): void;
+  find(cb: (node: T) => boolean, startingNode?: T): T;
+  filter(cb: (node: T, position: number) => boolean): LinkedList<T>;
+  head(): T;
   count(): number;
-  toArray(): LinkedListNode[];
+  toArray(): (T)[];
   isEmpty(): boolean;
   clear(): void;
-  static fromArray<T>(values: T[] | LinkedListNode[]): LinkedList<T>;
+  static fromArray<T extends LinkedListNode>(values: T[]): LinkedList<T>;
 }
